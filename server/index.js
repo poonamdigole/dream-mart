49a9fbbdcee01ed5d33be9632899f8bc820b5282
Poonam Digole <poonamdigole123@gmail.com>
2023-10-26 11:26:35 +0530

    built login and signup routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import User from "./models/user.js";
 import dotenv from "dotenv";
-
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000 ;
@@ -13,6 +14,61 @@ const connectDB = async () => {
         console.log(`MongoDB connected successfully` )
     }
 }
+
+// user signup 
+app.post('/signup', async (req, res) => {
+    const { name, address, mobile, email, password, gender } = req.body;
+    try {
+    const newUser = new User({
+         name,
+         address,
+         mobile,
+         email,
+         password,
+         gender
+    
+    })
+
+    const savedUser = await newUser.save();
+
+
+    res.json({
+        success: true,
+        data: savedUser,
+        message: 'successfully SignUp'
+    })
+   } 
+   catch(err) {
+    res.json({
+        success: false,
+        message: err.message
+    })
+
+   }
+});
+
+
+// user login
+app.get('/login', async (req, res) => {
+    const {email , password , mobile, name} = req.body;
+
+    const user = await User.findOne({email, password}).select('email name mobile');
+
+
+    if (user == null) {
+        return res.json({
+            success: false,
+            message: 'Login failed ! Please SignUp first'
+        })
+    }
+
+    res.json({
+        success: true,
+        data: user,
+        message: 'Login Sucessfully '
+    })
+}) ;
+
 
 app.listen(PORT , ()=> {
     console.log(`Server is running on ${PORT}`)
---
+++ server/models/user.js
@@ -0,0 +1,38 @@
+import {model , Schema} from 'mongoose';
+
+const userSchema = new Schema (
+    {
+      name :{
+        type : String,
+        default: "user"
+      },
+
+      address : {
+        type : String,
+        required : true
+      },
+
+      mobile : {
+        type : String,
+        required : true
+      },
+
+      email : {
+        type : String,
+        unique : true
+      },
+
+      password : {
+        type : String,
+        required : true
+      },
+
+      gender :{
+        type : String,
+        required : true
+      }
+    }
+) ;
+
+const User = model("user" , userSchema);
+export default User;
